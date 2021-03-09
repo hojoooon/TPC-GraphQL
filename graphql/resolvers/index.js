3d4df87fee6faad6a367c61c0468397d16a1a523
hojoooon <hohohoho0202@gmail.com>
2021-03-09 14:42:28 +0900

    Exception handled in resolvers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -10,9 +10,20 @@ async function getCookieData(domain, updatedDate){
   for(let i=0; i < data[0].cookieData.length; i++){
     if(data[0].cookieData[i].domain === domain && data[0].cookieData[i].updatedDate === updatedDate){
       target.push(data[0].cookieData[i])
+    }else if(data[0].cookieData[i].domain === domain && updatedDate === undefined){
+      target.push(data[0].cookieData[i])
+      target.sort((a, b) => {
+        if(parseInt(a.updatedDate) > parseInt(b.updatedDate)){
+          return -1;
+        }
+        if(parseInt(a.updatedDate) < parseInt(b.updatedDate)){
+          return 1;
+        }
+        return 0;
+      });
     }
   }
-  return target
+  return target.slice(0)
 }
 
 async function getPubData(publisher, updatedMonth){
@@ -22,9 +33,20 @@ async function getPubData(publisher, updatedMonth){
   for(let i=0; i < data[0].publisherData.length; i++){
     if(data[0].publisherData[i].publisher === publisher && data[0].publisherData[i].updatedMonth === updatedMonth){
       target.push(data[0].publisherData[i])
+    }else if(data[0].publisherData[i].publisher === publisher && updatedMonth === undefined){
+      target.push(data[0].publisherData[i])
+      target.sort((a, b) => {
+        if(parseInt(a.updatedMonth) > parseInt(b.updatedMonth)){
+          return -1;
+        }
+        if(parseInt(a.updatedMonth) < parseInt(b.updatedMonth)){
+          return 1;
+        }
+        return 0;
+      });
     }
   }
-  return target
+  return target.slice(0)
 }
 
 
--- models/index.js
+++ models/index.js
@@ -3,17 +3,7 @@ const mongoose = require('mongoose');
 const  {MongoClient}  = require('mongodb');
 const collectionName = 'data'
 const uri = `mongodb+srv://hojoon:hj0402@tpc-data.pog56.mongodb.net/TPCData?retryWrites=true&w=majority`
-// Connect to mongoDB
-// module.exports = () => {
-//   mongoose.connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   }).then(() => {
-//     console.log('MongoDB Connected')
-//   }).catch(err => {
-//     console.log(err);
-//   });
-// }
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -22,14 +12,7 @@ async function getData() {
   console.log('MongoDB Connected');
   const db = client.db('TPCData');
   const collection = db.collection(collectionName);
-  // console.log(await collection.find({}).toArray())
   return await collection.find({}).toArray();
 }
-// module.exports = () => {
-//   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-//   client.connect();
-//   const db = client.db(DB_NAME);
-//   const collection = db.collection(collectionName);
-//   return collection.find({}).toArray();
-// }
+
 module.exports = getData();
